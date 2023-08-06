1c1fb09f587f6e523aeae76e435993956f761346
0verGreen <141552030+0verGreen@users.noreply.github.com>
2023-08-06 16:25:21 +0700

    Add files via upload

---
+++ Web Potfolio/VSCODE/java.js
@@ -0,0 +1,8 @@
+<script type="module">
+  import { Toast } from 'bootstrap.esm.min.js'
+
+  var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel,{
+  KeyboardEvent=true
+})
+</script>
